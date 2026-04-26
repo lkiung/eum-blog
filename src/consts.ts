b5cecd5f226a174e1433f2a3510ea3458aa50501
kiwoong <lkiung@naver.com>
2026-04-26 14:44:14 +0900

    Fresh start: Blog only project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Eum Village AI';
+export const SITE_DESCRIPTION = '인공지능 기반 기업 분석 및 투자 인사이트 리포트';
